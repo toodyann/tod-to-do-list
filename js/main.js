d2fbea3df3b0120e92aacfa0f756d5e9c6db7a76
Konovalov Anatolii <mrdenth3@gmail.com>
2025-10-06 17:22:48 +0300

    add js code,beta v2.0

--- js/main.js
+++ js/main.js
@@ -22,4 +22,3 @@ button.addEventListener('click' , () => {
     list.removeChild (li);
     });
 });
-
---
+++ js/test.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', () => {
+    document.body.classList.add('loaded');
+  });
+  
