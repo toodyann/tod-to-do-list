9651866e83267dd01e712f0b1323e46cc1275b31
Konovalov Anatolii <mrdenth3@gmail.com>
2025-10-06 17:17:43 +0300

    fix js,beta v1.9

--- js/main.js
+++ js/main.js
@@ -4,19 +4,22 @@ const list = document.querySelector('.toDoList');
 
 button.addEventListener('click' , () => {
     const li = document.createElement('li');
-
     li.className = 'toDoListItem';
-    
+
+    const text = input.value.trim();
+    if (text === '') return;
+
     const deleteButton = document.createElement('button');
     li.innerText = input.value;
+
     deleteButton.innerText = 'Delete';
     deleteButton.className = 'deleteBtn';
     list.appendChild(li);
     li.appendChild(deleteButton);
     input.value = '';
-
+    
     deleteButton.addEventListener('click', () => {
-        list.removeChild (li);
+    list.removeChild (li);
     });
 });
 
